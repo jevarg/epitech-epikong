51833aff9332a90f8b92b417734fdfd7e802badf
Jean Gravier <jean.gravier@epitech.eu>
2014-03-07 21:44:12 +0100

    Added SDL and Makefile

---
+++ etape_1/main.c
@@ -0,0 +1,18 @@
+/*
+** main.c for rush in /home/gravie_j/Documents/projets/T2Rush1/etape_1
+**
+** Made by Jean Gravier
+** Login   <gravie_j@epitech.net>
+**
+** Started on  Fri Mar  7 21:22:45 2014 Jean Gravier
+** Last update Fri Mar  7 21:24:49 2014 Jean Gravier
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int		main(int argc, char *argv[])
+{
+  drawmap();
+  return (0);
+}
