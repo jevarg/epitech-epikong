593dcebcbdc146abb698a64c9bd9be1f1c30dd9d
Fritsch harold <harold.fritsch@epitech.eu>
2014-03-07 21:40:47 +0100

    ajout de epikong.h

---
+++ etape_1/epikong.h
@@ -0,0 +1,14 @@
+/*
+** epikong.h for  in /home/fritsc_h/projets/T2Rush1/etape_1
+** 
+** Made by Fritsch harold
+** Login   <fritsc_h@epitech.net>
+** 
+** Started on  Fri Mar  7 21:39:40 2014 Fritsch harold
+** Last update Fri Mar  7 21:40:17 2014 Fritsch harold
+*/
+
+#ifndef EPIKONG_H_
+# define EPIKONG_H_
+
+#endif /* !EPIKONG_H_ */
