d415132fe568cfb797c7491a003ca49f8b670f61
Fritsch harold <harold.fritsch@epitech.eu>
2014-03-08 16:43:32 +0100

    ajout du catch des touches simultanées

--- etape_2/main.c
+++ etape_2/main.c
@@ -5,32 +5,49 @@
 ** Login   <gravie_j@epitech.net>
 **
 ** Started on  Sat Mar  8 13:03:57 2014 Jean Gravier
-** Last update Sat Mar  8 13:26:36 2014 Jean Gravier
+** Last update Sat Mar  8 16:41:24 2014 Fritsch harold
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "epikong.h"
 
-void		pause(t_node *node)
+void		check_keys(t_node *node, Uint8 *keystates, int *stop)
+{
+  if (keystates[SDLK_ESCAPE])
+    *stop = 1;
+  else if (keystates[SDLK_LEFT] && keystates[SDLK_UP])
+    puts("jump left");//jump_left(node);
+  else if (keystates[SDLK_RIGHT] && keystates[SDLK_UP])
+    puts("jump right");//jump_right(node);
+  else if (keystates[SDLK_LEFT])
+    move_left(node);
+  else if (keystates[SDLK_RIGHT])
+    move_right(node);
+  else if (keystates[SDLK_UP])
+    puts("up");//move_up(node);
+  else if (keystates[SDLK_DOWN])
+    puts("down");//move_down(node);
+}
+
+void		sdl_loop(t_node *node)
 {
   int		stop;
   SDL_Event	event;
+  Uint8		*keystates;
 
   stop = 0;
+  keystates = SDL_GetKeyState(NULL);
   while (!stop)
     {
-      SDL_WaitEvent(&event);
+      SDL_PollEvent(&event);
+      usleep(100000);
       if (event.type == SDL_QUIT)
 	stop = 1;
-      else if (SDL_KEYDOWN)
+      else if (event.type == SDL_KEYDOWN)
 	{
-	  if (event.key.keysym.sym == SDLK_ESCAPE)
-	    stop = 1;
-	  else if (event.key.keysym.sym == SDLK_LEFT)
-	    move_left(node);
-	  else if (event.key.keysym.sym == SDLK_RIGHT)
-	    move_right(node);
+	  check_keys(node, keystates, &stop);
 	}
     }
 }
@@ -71,7 +88,7 @@ int		main(int argc, char *argv[])
       SDL_WM_SetCaption("Super Expendablos Deluxe edifion", NULL);
       set_position(s.map, s.character, 'i');
       draw_map(s.map, s.surface);
-      pause(&s);
+      sdl_loop(&s);
       SDL_Quit();
     }
   return (0);
